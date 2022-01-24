cca322b3d8decefc93bf5f02b0dddb5bbbbf401a
Bill Crosbie <billcrosbie@users.noreply.github.com>
2022-01-24 12:30:48 -0500

    init and draw sun2

--- Picture.java
+++ Picture.java
@@ -27,6 +27,7 @@ public class Picture
         window = new Square();
         roof = new Triangle();  
         sun = new Circle();
+        sun2 = new Circle();
         drawn = false;
     }
 
@@ -57,6 +58,9 @@ public class Picture
             sun.moveVertical(-40);
             sun.changeSize(80);
             sun.makeVisible();
+            
+            sun2.makeVisible();
+            
             drawn = true;
         }
     }
